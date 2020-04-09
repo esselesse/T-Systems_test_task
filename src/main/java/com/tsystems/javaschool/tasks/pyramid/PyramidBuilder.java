85b93e4116e67cff0da088a6b796cf64129a17b8
esselesse <wikkky@mail.ru>
2020-04-10 01:03:16 +0300

    'Pyramid' task passed tests.

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -12,10 +13,65 @@ public class PyramidBuilder {
      * @return 2d array with pyramid inside
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
+
+        if(inputNumbers.contains(null)) {
+            throw new CannotBuildPyramidException();
+        }
+
+        int counterOfNodes = 0;
+        int lenOfInputNumbers = inputNumbers.size();
+        int depthOfTheTree = 1;
+        while (counterOfNodes < lenOfInputNumbers){
+            counterOfNodes += depthOfTheTree;
+            // every next row has a number of numbers more than in current row by one
+            // so, the count of numbers in every row equal the index of that row
+            if (counterOfNodes > lenOfInputNumbers || counterOfNodes < 0) {
+                // well, if we have insufficient numbers to build a tree - or TOO MUCH, it is an exception
+                throw new CannotBuildPyramidException();
+            } else if(counterOfNodes == lenOfInputNumbers){
+                break;
+            }
+            depthOfTheTree++;
+        }
+        if(depthOfTheTree > Integer.MAX_VALUE/2) {
+            // just in case of emergency
+            throw new CannotBuildPyramidException();
+        }
+        int widthOfTheTree = 2 * depthOfTheTree - 1;
+        // because count of numbers in the last row is equal to 'depthOfTheTree';
+        // and count of zeros is less than count of numbers by 1.
+        // by the way, first number (on the top) is row['depthOfTheTree']
+        // so, we have matrix 'depthOfTheTree' X 'widthOfTheTree'
+        // first row contains only one number
+        // every next row has by one number more than previous
+
+        inputNumbers.sort((x, y) -> y - x);
+        // it is sorted in DEscending order because i want to fill final array from down to up from right to left
+        // unfortunately, i do not know how to simultaneously use lambdas for sorting and for catching duplicates
+        // catching them by two iterators
+        Iterator<Integer> iterator = inputNumbers.iterator();
+        Iterator<Integer> iterator1 = inputNumbers.iterator();
+        iterator.next();
+        while (iterator.hasNext()){
+            if(iterator.next().equals(iterator1.next())){
+                throw new CannotBuildPyramidException();
+            }
+        }
+
+        // finally, building tree array
+        // remember that by default it autofills by zeros
+        int[][] treeArray = new int[depthOfTheTree][widthOfTheTree];
+
+        int numbersCountInARow = depthOfTheTree;
+        iterator = inputNumbers.iterator();
+        for (int i = depthOfTheTree - 1; i >= 0 ; i--) {
+            for (int j = 0; j < numbersCountInARow; j++) {
+                treeArray[i][widthOfTheTree - 1 - depthOfTheTree + numbersCountInARow - 2 * j] = iterator.next();
+            }
+            numbersCountInARow--;
+        }
+
+        return treeArray;
     }
-
-
 }
